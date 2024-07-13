e968a9b5b5fecc5aaeb1e4f5d71d89cc48c6efac
jspuser123 <asp646@gmail.com>
2024-07-13 19:13:24 +0530

    spining clock add load img

--- optimisize_screen_loading/optimize_scr_kv_files/main.py
+++ optimisize_screen_loading/optimize_scr_kv_files/main.py
@@ -9,38 +9,73 @@ import threading
 from kivymd.uix.recyclegridlayout import MDRecycleGridLayout
 from kivy.clock import Clock,mainthread
 import time
+from kivymd.uix.boxlayout import MDBoxLayout ,BoxLayout
+from kivy.properties import *
+import socket
 
 class Grd(MDGridLayout):
 	pass
 class Txt1(MDTextField):
 	pass
+class Spin(MDBoxLayout):
+	value=NumericProperty(0)
+	def __init__(self, **kwargs):
+		super(Spin,self).__init__(**kwargs)
+		Clock.schedule_interval(self.spin_fun_start, 0.2/5)
+		self.val=0
+	def spin_fun_start(self,*args):
+		self.val+=1
+		self.value=self.val
+		if self.val == 100:
+			Clock.unschedule(self.spin_fun_start)
+
+      
 class S1(Screen):
     pass
 class S2(Screen):
 	def __init__(self, **kwargs):
 		super().__init__(**kwargs)
-		self.ids.dash_spin.active=True
-		threading.Thread(target=self.fun_start).start()
-		threading.Thread(target=self.th_fun).start()
-	def th_fun(self,*args):
-		for incre in range(200):
-			time.sleep(0.1/2)
-			self.ids.persentage.text=f'{incre}%'
-		self.ids.persentage.text=''
-	#@mainthread0
-	def fun_start(self,*args):
-		for uid in range(200):
-			time.sleep(0.1/2) 
-			Clock.schedule_once(lambda dt, uid=uid: self.add_wget(uid))
+		#self.ids.dash_spin.active=True
+		Clock.schedule_interval(self.add_wget, 0.2/5)
+		self.uid1=0
+	def add_wget(self, dt):
+		self.uid1+=1
+		x=Txt1(id=f'trims_{self.uid1}',hint_text='Particular',write_tab=False, max_text_length=9)
+		x1=Txt1(id=f'trims_int_{self.uid1}',hint_text='VALUE',write_tab=False, input_filter="float")
+		x.opacity=0;x1.opacity=0;self.ids.grd_id.add_widget(x);self.ids.grd_id.add_widget(x1);Animation(opacity=1,duration=.50).start(x);Animation(opacity=1,duration=.50).start(x1)
+		if self.uid1 ==39:
+			self.remove_widget(self.ids.spin_test)
+			self.list_gen()
+			Clock.unschedule(self.add_wget)
+			self.uid1=0
+	def list_gen(self):
+		self.l1=[]
+		self.l1=[f'trims_int_{i}' for i in range(40)]
 		self.ids.dash_spin.active=False
-	def add_wget(self, uid):
-		x=Txt1(id=f'textid{uid}',hint_text=f'textid{uid}');x.opacity=0;self.ids.grd_id.add_widget(x);Animation(opacity=1,duration=.50).start(x)
 	def id_getfun(self,*args):
-		# self.ids.grd_id.children[2].text
-		for co , x in enumerate(self.ids.grd_id.children):
-			if co == 2: 
-				print(x.id,x.text)
-				
+		self.ids.grd_id.children[x].text='1'
+		#total
+		total=0
+		data={}
+		data1={}
+		for val in self.ids.trimes_grid.children:
+			if val.id in self.l1:
+				if val.text != '':
+					total+=float(val.text)
+		#add
+		for val in self.ids.trimes_grid.children:
+			data[val.id]=val.text
+		#insert
+		for x in self.ids.trimes_grid.children:
+			if x.id == data1.key():
+				x.text=data1.val()
+		#update
+		for x in self.ids.trimes_grid.children:
+			data1[x.id]=x.text
+		#delete
+		for x in self.ids.trimes_grid.children:
+			x.text=''
+	
 
     
 
@@ -50,6 +85,7 @@ class Test(MDApp):
 		#Builder.load_string(KV)
 		Builder.load_file("loading.kv")
 		self.sm.add_widget(S1(name='s1'))
+		self.theme_cls.theme_style = "Light"
 		return self.sm
 	def on_start(self):
 		pass
@@ -64,10 +100,3 @@ class Test(MDApp):
 
 
 Test().run()
-
-# __dir__()
-# ['prop', 'obj', 'last_op', '__module__', '__init__', '__setitem__', '__delitem__', '__setslice__', '__delslice__', '__iadd__', '__imul__', 'append',
-#   'remove', 'insert', 'pop', 'extend', 'sort', 'reverse', '__dict__', '__weakref__', '__doc__', '__new__', '__repr__', '__hash__', '__getattribute__',
-#     '__lt__', '__le__', '__eq__', '__ne__', '__gt__', '__ge__', '__iter__', '__len__', '__getitem__', '__add__', '__mul__', '__rmul__', '__contains__', '__reversed__',
-# 	  '__sizeof__', 'clear', 'copy', 'index', 'count',
-#   '__class_getitem__', '__str__', '__setattr__', '__delattr__', '__reduce_ex__', '__reduce__', '__subclasshook__', '__init_subclass__', '__format__', '__dir__', '__class__']
